b4e5486417be3a406d8d7e4f279eea4d4585fdbf
Joaquin Salazar <joaqsala@gmail.com>
2023-03-20 12:37:07 -0500

    added initial fetch call for weather app - needs API key to work

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,26 @@
 
 
 
+function getWeather() {
+    //added class of city-input to input field in html
+    var cityInput = $("#city-input");
+    var city = cityInput.val()
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial"
+    
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      });
+    }
+
+
+
+
+
+
 function randomCocktail(){
     var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
@@ -15,11 +35,19 @@ function randomCocktail(){
           })
         .then(function (data) {
         console.log(data);
-
-
-
+        displayRandom(data);
          } )
-}
+};
+
+function displayRandom(cocktail) {
+    console.log(cocktail);
+    console.log(cocktail.drinks[0].strDrink)
+    console.log(cocktail.drinks[0].strDrinkThumb)
+    console.log(cocktail.drinks[0].strInstructions)
+
+
+
+    }
 
 randomCocktail()
 
