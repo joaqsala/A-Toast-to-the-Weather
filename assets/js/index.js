987d14d516c67c6aba8cbc70d2affb81665537f8
Joaquin Salazar <joaqsala@gmail.com>
2023-03-20 12:01:37 -0500

    added initial fetch call for a random cocktail

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,26 @@
-alert("working?")
 
 
 
 
+function randomCocktail(){
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.status !== 200){
+                console.log("There is an issue")
+                return;
+            }
+        return response.json();
+          })
+        .then(function (data) {
+        console.log(data);
 
 
 
-  
-  
+         } )
+}
+
+randomCocktail()
+
+
