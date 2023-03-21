8b2d8234b80185f154154bf97fb8b03104d32d2e
Joaquin Salazar <joaqsala@gmail.com>
2023-03-21 17:09:21 -0500

    added js to show a random cocktail

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
 
 
 function getWeather() {
-    //added class of city-input to input field in html
+    //added id of city-input to input field in html
     var cityInput = $("#city-input");
     var city = cityInput.val()
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial"
@@ -60,31 +60,51 @@ function getWeather() {
     ingredientVodka()
 
 
-// function randomCocktail(){
-//     var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+function randomCocktail(){
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
-//     fetch(queryURL)
-//         .then(function (response) {
-//             if (response.status !== 200){
-//                 console.log("There is an issue")
-//                 return;
-//             }
-//         return response.json();
-//           })
-//         .then(function (data) {
-//         console.log(data);
-//         displayRandom(data);
-//          } )
-// };
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.status !== 200){
+                console.log("There is an issue")
+                return;
+            }
+        return response.json();
+          })
+        .then(function (data) {
+        console.log(data);
+        displayRandom(data);
+         } )
+};
 
-// function displayRandom(cocktail) {
-//     console.log(cocktail);
-//     console.log(cocktail.drinks[0].strDrink)
-//     console.log(cocktail.drinks[0].strDrinkThumb)
-//     console.log(cocktail.drinks[0].strInstructions)
+function displayRandom(cocktail) {
+    console.log(cocktail);
+    console.log(cocktail.drinks[0].strDrink)
+    console.log(cocktail.drinks[0].strDrinkThumb)
+    console.log(cocktail.drinks[0].strInstructions)
 
-//     }
+    //in some div w/id of #drink-section
+    // var drinkDiv = $(#drink-section)
+    // var cocktailName = $('h2')
+    // cocktailName.html(cocktail.drinks[0].strDrink)
+    // drinkDiv.append(cocktailName)
 
-// randomCocktail()
+    // var img = $('img').src(cocktail.drinks[0].strDrinkThumb)  -- check jquery code?
+    // drinkDiv.append(cocktailName)
+
+    //for (var i = 1, i < 16, i++){
+    // if (cocktail.drinks[0][`strIngredients${i}` == null]){
+        // break;
+    // }
+    //  var ingredient = $('li').html(cocktail.drinks[0][`strMeasure${i}`]+ " : " + cocktail.drinks[0][`strIngredients${i}`] )
+    // drinkDiv.append(ingredient) 
+
+    //}
+ 
+        //var cocktailDirections = $('p').html(cocktail.drinks[0].strInstructions)
+        //drinkDiv.append(cocktailDirections)
+    }
+
+randomCocktail()
 
 
